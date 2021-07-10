0137b35ba5e264ccfd035cd6b4c94023eafc3386
aracelly <aracellybootcamp@gmail.com>
2021-07-09 23:04:53 -0400

    se eliminaron saltos de lineas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ let button = document.getElementById('play');
 
 button.onclick= function() {
 
-
   let requestNumber = () => {
     let num, error = false;
     do {
@@ -11,13 +10,10 @@ button.onclick= function() {
       num = parseInt(prompt("Ingrese un número entre 1 a 20 para realizar las operaciones."));
       error = true;
     } while (isNaN(num) || !Number.isInteger(num) || num > 20 || num < 1);
-
     return num;
   }
 
-
   let multiplication = (num) => {
-
     for (let i = 1; i <= num; i++) {
       console.log(`${i} x ${num} = ${i*num}`);
     }
@@ -25,7 +21,6 @@ button.onclick= function() {
   }
 
   let factorial = (num) => {
-
     for (let i = 1; i <= num; i++) {
       let fac = 1;
       for (let j = 1; j <= i; j++) {
