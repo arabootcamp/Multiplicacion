5b09d3a5a3bce527fca47b72486c08ef44df248f
aracelly <aracellybootcamp@gmail.com>
2021-07-09 21:51:27 -0400

    se añade carpeta opcionales con ejercicios adicionales

---
+++ 1-opcionales/assets/js/script-2.js
@@ -0,0 +1,34 @@
+let button1 = document.getElementById('play1');
+let button2 = document.getElementById('play2');
+
+button1.onclick = function () {
+
+  let i = 0;
+  let cont = 0;
+  do {
+    i++;
+    if (i == 16)
+      continue;
+    if (i == 45)
+      break;
+
+    if (i % 4 == 0)
+      console.log(`${i} es múltiplo de 4`);
+    else
+      cont++;
+
+  } while (i <= 50);
+
+  console.log(`La cantidad de números no múltiplos de cuatro es: ${cont}`)
+};
+
+button2.onclick = function () {
+
+    let change = (num) => {
+      msj = (num % 2 == 0) ? "Número Par" : "Número Impar";
+      alert(`El número ${num} es un ${msj}`);
+    }
+
+    change(Math.floor(Math.random()*1000));
+
+    };
